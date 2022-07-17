79316eb696aba566073b08997d0ac886dc686518
emad-almajdalawi <e.almajdalawi@gmail.com>
2022-07-17 23:48:31 +0300

    validation

--- auth/forms.py
+++ auth/forms.py
@@ -1,15 +1,31 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import Length, EqualTo, DataRequired, ValidationError
+from auth.models import User
 
 
 class SignupForm(FlaskForm):
-    username = StringField("Username")
+    def validate_email(self, check_email):
+        user_email = User.query.filter_by(email=check_email.data).first()
+        if user_email:
+            raise ValidationError("Email already exists")
+
+    def validate_username(self, check_username):
+        user_name = User.query.filter_by(username=check_username.data).first()
+        if user_name:
+            raise ValidationError("Username already exists")
+
+    username = StringField("Username", validators=[Length(min=3, max=20)])
     email = EmailField("Email")
-    password = PasswordField("Password")
+    password = PasswordField("Password", validators=[Length(min=5, max=20)])
+    password2 = PasswordField(
+        "Repeat Password", validators=[EqualTo("password"), DataRequired()]
+    )
     submit = SubmitField("Signup")
 
 
 class LoginForm(FlaskForm):
-    email = EmailField("Email")
+    username = StringField("User Name")
     password = PasswordField("Password")
     submit = SubmitField("Login")
--- auth/views.py
+++ auth/views.py
@@ -17,61 +17,52 @@ from auth.forms import SignupForm, LoginForm
 sign = blueprints.Blueprint("sign", __name__)
 
 
-@sign.route("/signup", methods=["GET"])
+@sign.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
-    return render_template("auth/signup.html", title="Sign Up", form=form)
+
+    if form.validate_on_submit():  # if form is submitted (POST)
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        if not username or not email or not password:
+            flash("Please fill all fields")
+            return redirect(url_for("sign.signup"))
+        new_user = User(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("sign.signin"))  # redirect to signin page
+
+    if form.errors != {}:
+        for error in form.errors:
+            flash(form.errors[error][0], category="danger")
+    return render_template("auth/signup.html", title="Sign Up", form=form)  # GET
 
 
-@sign.route("/signup", methods=["POST"])
-def signup_post():
-    username = request.form.get("username")
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not name or not email or not password:
-        flash("Please fill all fields")
-        return redirect(url_for("sign.signup_post"))
-
-    new_user = User(username, email, password)
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for("sign.signin"))
-
-    # form = SignupForm(request.form)
-    # if not form.validate():
-    #     flash("Please fill all fields")
-    #     return redirect(url_for("sign.signup_post"))
-
-
-@sign.route("/signin", methods=["GET"])
+@sign.route("/signin", methods=["GET", "POST"])
 def signin():
     form = LoginForm()
-    return render_template("auth/signin.html", title="Sign In", form=form)
+
+    if form.validate_on_submit():  # if form is submitted (POST)
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            flash("User not found")
+            return redirect(url_for("sign.signin"))
+        if user.password != password:
+            flash("Wrong password")
+            return redirect(url_for("sign.signin"))
+        session["user_id"] = user.id
+        return redirect(url_for("main.home"))  # redirect to home page
+
+    if form.errors != {}:
+        for error in form.errors:
+            flash(form.errors[error][0], category="danger")
+
+    return render_template("auth/signin.html", title="Sign In", form=form)  # GET
 
 
-@sign.route("/signin", methods=["POST"])
-def signin_post():
-    name = request.POST.get("name")
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    if not email or not password or not name:
-        flash("Please fill all fields")
-        return redirect(url_for("sign.signin_post"))
-
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        flash("User not found")
-        return redirect(url_for("sign.signin_post"))
-
-    if user.password != password:
-        flash("Wrong password")
-        return redirect(url_for("sign.signin_post"))
-
-    session["user_id"] = user.id
-    return redirect(url_for("main.home"))
-
-
-@sign.route("/signout", methods=["POST"])
+@sign.route("/signout")
 def signout():
     return redirect(url_for("sign.signin"))
